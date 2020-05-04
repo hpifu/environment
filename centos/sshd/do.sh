4e8e0abc88b79b59082ac0f02677de815fd3d3d6
hatlonely <hatlonely@gmail.com>
2020-05-04 20:27:08 +0800

    update

--- centos/sshd/do.sh
+++ centos/sshd/do.sh
@@ -1,6 +1,34 @@
 #!/usr/bin/env bash
 
-HOST=$1
+module=$(basename $(pwd))
 
-scp sshd_config root@${HOST}:/etc/ssh/sshd_config
-systemctl restart sshd
+function trac() {
+    echo -e "[TRAC] [$(date +"%Y-%m-%d %H:%M:%S")] $1"
+}
+
+function info() {
+    echo -e "[INFO] [$(date +"%Y-%m-%d %H:%M:%S")] \033[32m$1\033[0m"
+}
+
+function warn() {
+    echo -e "[WARN] [$(date +"%Y-%m-%d %H:%M:%S")] \033[31m$1\033[0m"
+}
+
+function check() {
+    diff sshd_config /etc/ssh/sshd_config
+}
+
+function install() {
+    check && trac "${module} alread exist. skip..." || {
+        cp sshd_config /etc/ssh/sshd_config &&
+        systemctl restart sshd &&
+        info "${module} install success" ||
+        warn "${module} install failed"
+    }
+}
+
+function main() {
+    install
+}
+
+main

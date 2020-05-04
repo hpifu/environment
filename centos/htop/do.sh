eb27a0143dcfb1d997eb1f0d3cec090a9a4441a7
zhicheng.lu <zhicheng.lu@mintegral.com>
2020-05-04 22:09:24 +0800

    htop

--- centos/htop/do.sh
+++ centos/htop/do.sh
@@ -18,6 +18,15 @@ function check() {
     hash htop 2>/dev/null
 }
 
+function epel() {
+    ls /etc/yum.repos.d/ | grep epel
+    if [ $? -ne 0 ]; then
+        wget -O /etc/yum.repos.d/epel-7.repo http://mirrors.aliyun.com/repo/epel-7.repo &&
+        yum clean all &&
+        yum makecache &&
+    f
+}
+
 function install() {
     check && trac "${module} alread exist. skip..." || {
         yum install -y htop &&

5e805425d409c7cf96defef8410547d612c46f80
hatlonely <hatlonely@gmail.com>
2020-05-04 21:38:35 +0800

    update

--- centos/zsh/do.sh
+++ centos/zsh/do.sh
@@ -14,17 +14,46 @@ function warn() {
     echo -e "[WARN] [$(date +"%Y-%m-%d %H:%M:%S")] \033[31m$1\033[0m"
 }
 
-function check() {
+function check_zsh() {
     hash zsh 2>/dev/null
 }
 
-function install() {
-    check && trac "${module} alread exist. skip..." || {
+function install_zsh() {
+    check_zsh && trac "${module} zsh alread exist. skip..." || {
         yum install -y zsh &&
         sh oh-my-zsh.sh &&
         info "${module} install success" ||
         warn "${module} install failed"
     }
+}
+
+function check_oh_my_zsh() {
+    test -e ~/.oh-my-zsh
+}
+
+function install_oh_my_zsh() {
+    check_oh_my_zsh && trac "${module} oh-my-zsh alread exist. skip..." || {
+        sh oh-my-zsh.sh &&
+        info "${module} oh-my-zh install success" ||
+        warn "${module} oh-my-zh install failed"
+    }
+}
+
+function check_zshrc() {
+    diff ~/.zshrc zshrc
+}
+
+function install_zshrc() {
+    check_zshrc && trac "${module} zshrc no diff. skip..." || {
+        sh oh-my-zsh.sh &&
+        info "${module} zshrc update success" ||
+        warn "${module} zshrc update failed"
+    }
+}
+
+function install() {
+    install_zsh
+    install_oh_my_zsh
 }
 
 function main() {

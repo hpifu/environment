2c2b98470e188d6c5304a6108e8989e4b4f0a61a
hatlonely <hatlonely@gmail.com>
2020-05-04 20:34:00 +0800

    update

--- centos/init.sh
+++ centos/init.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
 
+workroot=$(pwd)
+
+function trac() {
+    echo -e "[TRAC] [$(date +"%Y-%m-%d %H:%M:%S")] $1"
+}
+
+function info() {
+    echo -e "[INFO] [$(date +"%Y-%m-%d %H:%M:%S")] \033[32m$1\033[0m"
+}
+
+function warn() {
+    echo -e "[WARN] [$(date +"%Y-%m-%d %H:%M:%S")] \033[31m$1\033[0m"
+}
+
 function main() {
     for module in $(ls); do
-        cd $module && sh do.sh
+        cd ${workroot}
+        trac "==================================="
+        trac "do ${module}"
+        cd ${module} && sh do.sh
     done
 }
 
